095a8df6b4cc312caa1b20823a4eece9c1dd3f3a
Ronan Breen <X00152190@mytudublin.ie>
2020-03-11 19:49:46 +0000

    Add project files.

---
+++ GolfClient/Model/Golfer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GolfClub.Models
+{
+    public enum MemberType { Youth, FullTime, PartTime, Patron, Student };
+
+    public class Golfer
+    {
+        public int GUI { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Surname { get; set; }
+
+        public MemberType Membership { get; set; }
+
+        public int Handicap { get; set; }
+
+        public DateTime DateJoined { get; set; }
+
+        public int YearlyFees { get; set; }
+
+        public override string ToString()
+        {
+            return "\nFirst Name: " + this.FirstName + "\nSurname: " + this.Surname + "\nGUI: " + this.GUI + "\nMembership Type: " + this.Membership + "\nHandicap: " + this.Handicap + "\nYearly Fees: " + this.YearlyFees + "\nDate Joined: " + this.DateJoined;
+
+        }
+    }
+}
---
+++ GolfClient/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using GolfClub.Models;
+
+namespace GolfClient
+{
+    class Program
+    {
+        static async Task GetsAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:62925/api/GolfClub/");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("applications/json"));
+
+                // GET: api/GolfClub/all
+                HttpResponseMessage response = await client.GetAsync("all");
+                if (response.IsSuccessStatusCode)
+                {
+                    var returnedGolfers = await response.Content.ReadAsAsync<IEnumerable<Golfer>>(); // add from Console line
+                    Console.WriteLine("*** Complete Golfer List ***");
+                    foreach (Golfer g in returnedGolfers)
+                    {
+                        Console.WriteLine(g);
+                    }
+                }
+
+                // GET: api/GolfClub/GetByGUI/11111111
+                response = await client.GetAsync("GetByGUI/11111111");
+                if (response.IsSuccessStatusCode)
+                {
+                    var returnedGolfer = await response.Content.ReadAsAsync<Golfer>(); // add from Console line
+                    Console.WriteLine("\n*** Golfer with GUI of 11111111 is ***");
+                    Console.WriteLine(returnedGolfer);
+                }
+
+
+                // POST: api/GolfClub/AddMember
+                Golfer g1 = new Golfer() { GUI = 55555555, FirstName = "Andrew", Surname = "Breen", Handicap = 45, DateJoined = new DateTime(2020, 02, 02), Membership = MemberType.Patron, YearlyFees = 2000 };
+                response = client.PostAsJsonAsync("AddMember", g1).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("\nAdded Golfer: " + g1.FirstName + " " + g1.Surname); 
+                }
+
+
+                // PUT: api/GolfClub/UpdateMember/11111111
+                Golfer g2 = new Golfer() { GUI = 11111111, FirstName = "Rick", Surname = "Ross", Handicap = 3, DateJoined = new DateTime(2020, 02, 02), Membership = MemberType.Patron, YearlyFees = 2000 };
+                response = client.PutAsJsonAsync("UpdateMember/11111111", g2).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("\nUpdated Golfer: " + g2.FirstName + " " + g2.Surname);
+                }
+
+
+                // DELETE: api/GolfClub/DeleteMember/33333333
+                Golfer g3 = new Golfer() { GUI = 11111111, FirstName = "Rick", Surname = "Ross", Handicap = 3, DateJoined = new DateTime(2020, 02, 02), Membership = MemberType.Patron, YearlyFees = 2000 };
+                response = client.DeleteAsync("DeleteMember/33333333").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("\nDeleted Golfer: " + g3.FirstName + " " + g3.Surname);
+                }
+            }
+        }
+
+        static void Main()
+        {
+            GetsAsync().Wait();
+            Console.ReadLine();
+        }
+    }
+}
